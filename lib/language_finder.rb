3e08037b92ac8616c6f59803277ef74f6877f51e
Jon Shad <shad_stick@hotmail.co.uk>
2017-07-02 20:31:28 +0100

    added tests for non-existent user and user without repos. Tests passing.

--- lib/language_finder.rb
+++ lib/language_finder.rb
@@ -8,7 +8,7 @@ class LanguageFinder
   end
 
   def top_language
-    if input_handler == true
+    if no_input_check == true
       language_list = get_languages
       top_lang = language_list.last
       "#{top_lang[0]} is used most frequently by #{@user} - #{top_lang[1]} times in fact."
@@ -23,13 +23,15 @@ class LanguageFinder
 
   def get_api_data
     response = Faraday.get(github_url)
+    check_status(response.headers)
     JSON.parse(response.body)
   end
 
   def get_languages
-    repos = get_api_data
     languages = {}
 
+    repos = get_api_data
+    check_for_repos(repos)
     repos.each do |repo|
       lang = repo['language']
       languages.include?(lang) ? languages[lang] += 1 : languages[lang] = 1
@@ -37,20 +39,32 @@ class LanguageFinder
     languages.sort_by{|lang,count| count }
   end
 
-  def input_handler
+  def no_input_check
     if @user == nil
-      begin
-        raise StandardError
-      rescue
-        puts INPUT_ERRORS[:no_user]
-      end
+      puts INPUT_ERRORS[:no_user]
     else
       true
     end
   end
 
+  def check_status(api_header)
+    if api_header['status'] == '404 Not Found'
+      puts INPUT_ERRORS[:invalid_user]
+      exit
+    end
+  end
+
+  def check_for_repos(repo_obj)
+    if repo_obj.empty?
+      puts INPUT_ERRORS[:no_repos]
+      exit
+    end
+  end
+
   INPUT_ERRORS = {
-    no_user: "Please enter a Github username after launching app - eg. 'ruby lang.rb username'"
+    no_user: "Please enter a Github username after launching app - eg. 'ruby lang.rb username'.",
+    invalid_user: "That user does not seem to exist, please try again.",
+    no_repos: "That user does not seem to have any repos."
   }
 
 end
--- spec/language_finder_spec.rb
+++ spec/language_finder_spec.rb
@@ -2,7 +2,7 @@ require "language_finder.rb"
 
 describe LanguageFinder do
 
-  context "app works as intended" do
+  context "When a valid user is entered" do
     it "Should output best guess at main language" do
 
       stub_request(:get, "https://api.github.com/users/testuser/repos").
@@ -18,14 +18,41 @@ describe LanguageFinder do
           }]',headers: {})
 
       testuser = LanguageFinder.new('testuser')
-      EXPECTED_OUTPUT = "Ruby is used most frequently by testuser - 2 times in fact."
-      expect(testuser.top_language).to eq(EXPECTED_OUTPUT)
+      RUBY_USED = "Ruby is used most frequently by testuser - 2 times in fact."
+      expect(testuser.top_language).to eq(RUBY_USED)
     end
+  end
 
-    it "should return 'provide username' prompt if username not given" do
+  context "When no username is provided" do
+    it "should return 'provide username' prompt" do
       blank = LanguageFinder.new(nil)
-      ERROR = "Please enter a Github username after launching app - eg. 'ruby lang.rb username'\n"
+      ERROR = "Please enter a Github username after launching app - eg. 'ruby lang.rb username'.\n"
       expect{blank.top_language}.to output(ERROR).to_stdout
     end
   end
+
+  context "When a non-existent user is provided" do
+    it "should return 'invalid user' prompt" do
+
+      stub_request(:get, "https://api.github.com/users/invaliduser/repos").
+        to_return(status: 404, headers: {"status": "404 Not Found"})
+
+      invalid = LanguageFinder.new('invaliduser')
+      NO_USER = "That user does not seem to exist, please try again."
+      expect{invalid.top_language}.to output(NO_USER).to_stdout
+    end
+  end
+
+  context "When a provided user has no repositories" do
+    it "Should return 'no repos' prompt" do
+
+      stub_request(:get, "https://api.github.com/users/norepos/repos").
+        to_return(status: 200, body: '[]',headers: {})
+
+      no_repos = LanguageFinder.new('norepos')
+      NO_REPOS = "That user does not seem to have any repos."
+      expect{no_repos.top_language}.to output(NO_REPOS).to_stdout
+    end
+  end
+
 end
